c0dd8c87c0bd28dd607f6204f88ce6c23619e17e
Tavbulatov <alikhantam26@gmail.com>
2023-03-29 16:40:58 +0300

    rubocop work

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_120643) do
-
+ActiveRecord::Schema.define(version: 20_230_329_120_643) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "answers", force: :cascade do |t|
-    t.bigint "question_id", null: false
-    t.text "body", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
+  create_table 'answers', force: :cascade do |t|
+    t.bigint 'question_id', null: false
+    t.text 'body', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'questions', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'body', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions"
+  add_foreign_key 'answers', 'questions'
 end

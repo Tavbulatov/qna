cc9524fe6006901c13e1c19e49ce3bbff7df58b8
Tavbulatov <alikhantam26@gmail.com>
2023-04-27 14:54:15 +0300

    writing tests for models: user, question, answer for associations and validation of user data
    test requirements implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :questions, foreign_key: :author_id, dependent: :destroy
+  has_many :answers, foreign_key: :author_id, dependent: :destroy
+
+  validates :last_name, :first_name, presence: true
 end

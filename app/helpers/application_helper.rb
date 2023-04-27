004f35f1db0b502a962cab2ad28e58ada22d8524
Tavbulatov <alikhantam26@gmail.com>
2023-04-27 13:26:14 +0300

    creating partial for reading flash messages and render to layout, creating helper classes for flash message types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_message_css_class(key)
+    case key.to_sym
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-error'
+    when :alert then 'alert alert-danger'
+    end
+  end
 end

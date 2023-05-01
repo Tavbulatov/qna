ad1733f39e2308a404661a3ed543d6f854898b72
Tavbulatov <alikhantam26@gmail.com>
2023-05-01 18:32:11 +0300

    implementation of the destroy action with notifications added and a filter added to set the question variable and authenticate the user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,34 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
+  before_action :find_question, only: %i[show destroy]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
-    @answer = @question.answers.new
   end
 
   def new
-    @question = Question.new
+    @question = current_user.questions.new
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
+    end
+  end
+
+  def destroy
+    if @question.author == current_user
+      @question.destroy
+      redirect_to questions_path, notice: 'Your question has been successfully deleted'
+    else
+      redirect_to questions_path, alert: "You can't delete someone else's question"
     end
   end
 
@@ -25,5 +36,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end

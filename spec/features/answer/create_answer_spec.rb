6987d27679aac86f6a5fa1e3a4cbf7a1cc486fcc
Tavbulatov <alikhantam26@gmail.com>
2023-05-05 00:30:05 +0300

    removed question creation script on the questions page

--- spec/features/answer/create_answer_spec.rb
+++ spec/features/answer/create_answer_spec.rb
@@ -9,11 +9,9 @@ feature 'User can create answer', '
   given!(:question) { create(:question) }
 
   describe 'Authenticated user' do
-    background do
-      sign_in(user)
-    end
-
     scenario 'creating an answer on the question page' do
+      sign_in(user)
+
       visit question_path(question)
 
       fill_in 'Body', with: 'answer to question'
@@ -22,23 +20,6 @@ feature 'User can create answer', '
       expect(page).to have_content('answer to question')
       expect(page).to have_content('Answer created successfully')
     end
-
-    scenario 'answering a question on the all questions page' do
-      click_on 'Answer the question'
-
-      fill_in 'Body', with: 'answer to question'
-      click_on('Create Answer')
-
-      expect(page).to have_content('answer to question')
-      expect(page).to have_content('Answer created successfully')
-    end
-  end
-
-  scenario 'Unauthenticated user is trying to answer a question' do
-    visit questions_path
-    click_on 'Answer the question'
-
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
   scenario 'unauthenticated user creates an answer on the question page' do

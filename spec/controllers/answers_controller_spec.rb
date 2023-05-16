e0e5baa4086ed5f71abd2ab11d773d41ce6f5795
Tavbulatov <alikhantam26@gmail.com>
2023-05-17 01:47:30 +0300

    changes in test controller answer
    added js format: true
    Removed display methods, new one.
    Added update methods and changed create method

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,44 +3,20 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
   let(:question) { create(:question, author: user) }
-
-  describe 'GET #new' do
-    before { login(user) }
-
-    before { get :new, params: { question_id: question } }
-
-    it 'assigns a new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
-  describe 'GET #show' do
-    let(:answer) { create(:answer) }
-
-    it 'assigning a variable to view the answer' do
-      get :show, params: { question_id: question, id: answer }
-      expect(assigns(:answer)).to eq(answer)
-    end
-  end
+  let!(:answer) { create(:answer, question: question, author: user) }
 
   describe 'POST #create' do
     before { login(user) }
 
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
-        expect do
-          post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        end.to change(Answer, :count).by(1)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) }, format: :js }.to change(Answer, :count).by(1)
       end
 
-      before { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
+      before { post :create, params: { question_id: question, answer: attributes_for(:answer) }, format: :js  }
 
-      it 'redirect to show' do
-        expect(response).to redirect_to(question_answer_path(question, Answer.last))
+      it 'render create view' do
+        expect(response).to render_template :create
       end
 
       it 'sets a flash message' do
@@ -50,21 +26,59 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid attributes' do
       it 'answer not saved' do
-        expect do
-          post :create,
-               params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        end.to_not change(Answer, :count)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }, format: :js  }.to_not change(Answer, :count)
       end
 
-      it 'render new view' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        expect(response).to render_template :new
+      it 'render create view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }, format: :js
+        expect(response).to render_template :create
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    before { login(user) }
+    before { patch :update, params: {id: answer, answer: {body: 'answer'} }, format: :js }
+
+    context 'with valid attributes' do
+      it 'Edit body' do
+        answer.reload
+        expect(answer.body).to eq('answer')
+      end
+
+      it 'render update view' do
+        expect(response).to render_template :update
+      end
+
+      it 'sets a flash message' do
+        expect(flash[:notice]).to eq('Answer edited successfully')
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'answer not saved' do
+        expect { patch :update, params: {id: answer, answer: attributes_for(:answer, :invalid) },format: :js }.to_not change(answer, :body)
+      end
+
+      it 'render update view' do
+        patch :update, params: {id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context "Not the author trying to edit" do
+      let(:other_user) { create(:user) }
+
+      before { login(other_user) }
+
+      it 'Title attribute has not changed' do
+        expect {patch :update, params: {id: answer, answer: {body: 'answer'} }, format: :js}.to_not change(answer, :body)
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    let!(:answer) { create(:answer, question: question, author: user) }
+
 
     before { login(user) }
 

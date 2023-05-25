6963bf6a18324e6dd6149828444cb6532896b599
Tavbulatov <alikhantam26@gmail.com>
2023-05-25 13:23:09 +0300

    added a :trait to factori_bot for adding files

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -7,5 +7,13 @@ FactoryBot.define do
     trait :invalid do
       body { nil }
     end
+
+    trait :with_file do
+      after(:build) do |answer|
+        answer.files.attach(io: File.open(Rails.root.join('spec', 'rails_helper.rb')), filename: 'rails_helper.rb', content_type: 'application/ruby')
+      end
+    end
+
+    factory :answer_with_file, traits: [:with_file]
   end
 end
